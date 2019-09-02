803769934d344090037d4d1a776e9f82985706f3
jficerai <julien.ficerai@boursorama.fr>
2019-09-02 15:11:13 +0200

    remove demo code

--- Taylor/Controller/ViewController.swift
+++ Taylor/Controller/ViewController.swift
@@ -9,18 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    fileprivate func extractedFunc() -> Human {
-        return Human(name: "Steve", surname: "Jobs", gender: Gender.male ,lifePoint: 120, skillsPoints: [
-            Skills.speed : 20,
-            Skills.intelligence: 25,
-            Skills.agility : 5])
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let steveJobs = extractedFunc()
     }
 }
 
