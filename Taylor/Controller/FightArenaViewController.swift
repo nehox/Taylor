b49c86d8f54a8a07bd802c3d1e80af76ecdd201d
jficerai <julien.ficerai@boursorama.fr>
2019-09-02 16:13:44 +0200

    add monster and fightArena

--- Taylor/Controller/FightArenaViewController.swift
+++ Taylor/Controller/FightArenaViewController.swift
@@ -7,3 +7,64 @@
 //
 
 import Foundation
+import UIKit
+
+class FightArenaViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //create player as Human
+        let player = Archer(name: "John", surname: "Doe", gender: .male, weapon: Weapon(type: .melee, damage: 20))
+        
+        //create monster as Zombi
+        let monster = Zombi()
+        
+        
+        //do fight
+        round(player: player, monster: monster)
+        round(player: player, monster: monster)
+        round(player: player, monster: monster)
+        
+        //regen human
+        player.eat()
+        player.eat()
+        player.eat()
+        
+        //finish fight
+        round(player: player, monster: monster)
+        round(player: player, monster: monster)
+    }
+    
+    
+    /// do a round
+    /// - Parameter player: Human
+    /// - Parameter monster: Zombi
+    func round(player: Human, monster: Zombi) {
+     
+        switch player  {
+            case is Archer:
+                monster.lifePoint -= (player as! Archer).weapon?.damage ?? 10
+                break
+            case is Magician:
+                monster.lifePoint -= (player as! Magician).weapon?.damage ?? 10
+                break
+            case is Thief:
+                monster.lifePoint -= (player as! Thief).weapon?.damage ?? 10
+                break
+            default:
+                print("not possible ! ")
+        }
+        
+        
+        if monster.lifePoint <= 0 {
+            print("Zombi died")
+        } else {
+            player.lifePoint -= monster.damage
+        }
+        
+        print("Zombi life : \(monster.lifePoint)\nPlayer life : \(player.lifePoint)")
+        
+        
+    }
+}
--- Zombi.swift
+++ Zombi.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class Zombi {
     
-    let lifePoint: Int
+    var lifePoint: Int
     let damage: Int
     let hitChance: Int
     
