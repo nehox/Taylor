4d778a0663e16afa8207f362c911edbc9bb89b87
Julien Ficerai <julien.ficerai@oika.eu>
2019-09-03 23:17:29 +0200

    get data in coreData and populate cell

--- Taylor/Controller/CharacterListViewController.swift
+++ Taylor/Controller/CharacterListViewController.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import CoreData
 
 class CharacterListViewController: UIViewController {
     
@@ -16,6 +17,7 @@ class CharacterListViewController: UIViewController {
     @IBOutlet weak var listCharacterTableView: UITableView!
     
     override func viewDidLoad() {
+        initData()
         
     }
     
@@ -23,18 +25,71 @@ class CharacterListViewController: UIViewController {
     @IBAction func closeViewButton(_ sender: Any) {
         self.dismiss(animated: true)
     }
+    
+    private func initData(){
+        do {
+            let request: NSFetchRequest<Player> = Player.fetchRequest()
+            let players = try AppDelegate.viewContext.fetch(request)
+            
+            for player in players {
+                switch player.playerClass {
+                case "Adventurer":
+                    listCharacters.append(Adventurer(name: player.name ?? "",
+                                                     surname: player.surname ?? "",
+                                                     gender: getGender(gender: player.gender ?? ""),
+                                                     weapon: nil)
+                    )
+                    break
+                case "Ninja":
+                    listCharacters.append(Ninja(name: player.name ?? "",
+                                                surname: player.surname ?? "",
+                                                gender: getGender(gender: player.gender ?? ""),
+                                                weapon: nil)
+                                       )
+                    break
+                case "Zombi":
+                    listCharacters.append(Zombi(name: player.name ?? "",
+                                                surname: player.surname ?? "",
+                                                gender: getGender(gender: player.gender ?? ""),
+                                                weapon: nil)
+                                       )
+                    break
+                default:
+                    listCharacters.append(Human(name: player.name ?? "",
+                                                surname: player.surname ?? "",
+                                                gender: getGender(gender: player.gender ?? ""))
+                                       )
+                    
+                }
+            }
+            
+        } catch {
+            print("Ooups on a pas vraiment reussi a récup les players ...")
+        }
+    }
+    
+    private func getGender(gender: String) -> Gender {
+        switch gender {
+        case "Male":
+            return .male
+        case "Female":
+            return .female
+        default:
+            return .other
+        }
+    }
 }
 
 extension CharacterListViewController : UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 8
+        return listCharacters.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = UITableViewCell()
         
-        cell.textLabel?.text = "coucou"
+        cell.textLabel?.text = listCharacters[indexPath.row].name
         
         return cell
     }
